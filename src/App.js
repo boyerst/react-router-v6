d7f5c91f4e2c5f837e13c3da8613dfc808fe8893
Steve Boyer <stephen.a.boyer@gmail.com>
2023-08-11 14:06:33 -0400

    Add Posts component with nested PostsList component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom';
+
+
+const BlogPosts = {
+  'first-blog-post': {
+    title: 'First Blog Post',
+    description: 'Lorem ipsum dolor sit amet, consectetur adip.'
+  },
+  'second-blog-post': {
+    title: 'Second Blog Post',
+    description: 'Hello React Router v6'
+  }
+};
+
 
 
 function NoMatch() {
@@ -27,19 +40,28 @@ function App() {
     <div className="App">
      
       <Router>
+
         <nav style={{ margin: 10 }}>
           <Link to="/" style={{ padding: 5 }}>
           Home
+          </Link>
+          <Link to="/posts" style={{ padding: 5 }}>
+          Posts
           </Link>
           <Link to="/about" style={{ padding: 5 }}>
           About
           </Link>
         </nav>
+
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="/posts" element={<Posts />}>
+            <Route index element={<PostLists />} />
+          </Route>  
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NoMatch />} />
         </Routes>
+
       </Router>
     </div>
   );
@@ -54,6 +76,39 @@ function About() {
     </div>
   );
 }
+
+
+function Posts() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Blog</h2>
+      <Outlet />
+    </div>
+  );
+}
+
+
+function PostLists() {
+  return (
+    <ul>
+      {Object.entries(BlogPosts).map(([slug, { title }]) => (
+        <li key={slug}>
+          <h3>{title}</h3>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
