65741266673b82de8b27aee39589f42e0250958d
Steve Boyer <stephen.a.boyer@gmail.com>
2023-08-11 13:08:50 -0400

    Add Home and About components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+
+
+function Home() {
   return (
-    <div className="App">
-  
-    <Router>{/* All routes are nested inside it */}</Router>;
+    <div style={{ padding: 20 }}>
+      <h2>Home View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
     </div>
   );
 }
+
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+
+function About() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>About View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+
+
+
+
 
 export default App;
